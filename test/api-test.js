67c277b8e0c1e9b04c21673af6db0178e1df3382
Fedor Indutny <fedor.indutny@gmail.com>
2011-10-09 02:08:41 +0700

    [initial]

---
+++ test/api-test.js
@@ -0,0 +1,91 @@
+var vows = require('vows'),
+    assert = require('assert');
+
+var deadbolt = require('../lib/deadbolt');
+
+var d;
+
+function gc() {
+  return setInterval(function () {
+    var x = new Array(100000).join('x');
+  }, 1000);
+};
+
+vows.describe('deadbolt/api').addBatch({
+  'Calling deadbolt.create()': {
+    topic: function () {
+      d = deadbolt.create();
+      return null;
+    },
+    'should create deadbolt instance with memory storage': function () {}
+  }
+}).addBatch({
+  'Calling d.lock(id, callback)': {
+    topic: function() {
+      d.lock('a', this.callback);
+    },
+    'should return object': {
+      'with .unlock method': function(err, o) {
+        assert.isNull(err);
+        assert.include(o, 'unlock');
+      },
+      'and if calling it\'s .unlock(callback) method': {
+        topic: function(o) {
+          o.unlock(this.callback);
+        },
+        'should call callback': function(err, data) {
+          assert.isNull(err);
+        },
+        'and finally calling d.lock again': {
+          topic: function() {
+            d.lock('a', this.callback);
+          },
+          'should be successful': function (err, data) {
+            assert.isNull(err);
+          }
+        }
+      }
+    }
+  }
+}).addBatch({
+  'Calling d.lock(id, callback)': {
+    topic: function() {
+      d.lock('b', this.callback);
+    },
+    'should return object': {
+      'with .unlock method': function(err, o) {
+        assert.isNull(err);
+        assert.include(o, 'unlock');
+      },
+      'and calling d.lock(id, callback) twice': {
+        topic: function() {
+          d.lock('b', this.callback);
+        },
+        'should return error': function(err, data) {
+          assert.ok(err);
+        }
+      }
+    }
+  }
+}).addBatch({
+  'Calling d.lock(id, nop).report(callback)': {
+    topic: function() {
+      var callback = this.callback;
+      d.lock('c', function() {}).report(function(err) {
+        callback(null, err);
+      });
+      this.int = gc();
+    },
+    'should emit reporter callback with error': function(err) {
+      clearInterval(this.int);
+      assert.ok(err);
+    },
+    'and creating same lock again': {
+      topic: function () {
+        d.lock('c', this.callback);
+      },
+      'should be without errors': function () {
+      }
+    }
+  }
+}).export(module);
