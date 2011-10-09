7bb784ae416133722deb6ffeccce21e0a67c6ac1
Fedor Indutny <fedor.indutny@gmail.com>
2011-10-09 20:48:52 +0700

    [test] added test for wrap

---
+++ test/wrap-test.js
@@ -0,0 +1,38 @@
+var vows = require('vows'),
+    assert = require('assert');
+
+var deadbolt = require('../lib/deadbolt');
+
+function doGc() {
+  return setInterval(function () { gc(); }, 200);
+};
+
+vows.describe('deadbolt/api').addBatch({
+  'Calling deadbolt.wrap for callback': {
+    'that fires': {
+      topic: function () {
+        var callback = deadbolt.wrap(this.callback);
+        this.int = gc();
+
+        setTimeout(function () {
+          callback(null);
+        }, 1000);
+      },
+      'should not fire weak error': function (err, data) {
+        assert.isNull(err);
+      }
+    }
+  }
+}).addBatch({
+  'Calling deadbolt.wrap for callback': {
+    'that is not firing': {
+      topic: function () {
+        var callback = deadbolt.wrap(this.callback);
+        this.int = gc();
+      },
+      'should fire weak error': function (err, data) {
+        assert.ok(err);
+      }
+    }
+  }
+}).export(module);
